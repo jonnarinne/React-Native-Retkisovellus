cec6ffc81b3be7046350cd090b573a28f17efd06
Jonna Rinne <jonna.rinne1@gmail.com>
2024-11-11 17:21:34 +0200

    firebaseConfig.js luotu

---
+++ firebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC__rhjZjQsDpJK73t990MyQjcoAVdXrwE",
+  authDomain: "retkisovellus.firebaseapp.com",
+  databaseURL: "https://retkisovellus-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "retkisovellus",
+  storageBucket: "retkisovellus.firebasestorage.app",
+  messagingSenderId: "982248131505",
+  appId: "1:982248131505:web:4cafd039940151c7400b90",
+  measurementId: "G-P98EMFFF7X"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
+
+
+
